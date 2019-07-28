b26eec9c6537a2c66cef4c830122324cddf2d0a1
sa <szymon.apolinarski@gmail.com>
2019-07-28 20:19:18 +0200

    main-script added

---
+++ main-script.js
@@ -0,0 +1,56 @@
+function initializeClient(address, username, password) {
+    var mantisInTask = document.getElementById('customfield_10904-val');
+    var mantisNumbers = mantisInTask.innerHTML.split(',');
+    for(var j=0; j < mantisNumbers.length; j++) {
+        var number = mantisNumbers[j].match(/\d+/)[0];
+        if (number !== undefined && !isNaN(number)) {
+            client = new MantisClient(address, username, password, number);
+            break;
+        }
+    }
+}
+
+function addListenerToEdit(fieldId) {
+    var inactiveSpan = document.getElementById(fieldId + "-val-inactive");
+    var inactiveSpanInner = document.getElementById(fieldId + "-val-inactive-inner");
+    var submitBtn = document.getElementById(fieldId + "-btn-submit");
+    var cancelBtn = document.getElementById(fieldId + "-btn-cancel");
+
+    inactiveSpan.addEventListener("click", function() {
+        var spanInactive = document.getElementById(fieldId + "-val-inactive");
+        var spanEdit = document.getElementById(fieldId + "-form");
+
+        spanInactive.style.display = "none";
+        spanEdit.style.display = "";
+    }, false);
+
+    inactiveSpanInner.addEventListener("click", function() {
+        var spanInactive = document.getElementById(fieldId + "-val-inactive");
+        var spanEdit = document.getElementById(fieldId + "-form");
+
+        spanInactive.style.display = "none";
+        spanEdit.style.display = "";
+    }, false);
+
+    submitBtn.addEventListener("click", function() {
+        var spanInactive = document.getElementById(fieldId + "-val-inactive");
+        var spanEdit = document.getElementById(fieldId + "-form");
+
+
+        var editTask = new EditTask(client.taskId);
+        editTask[fieldId] = spanEdit[0].value;
+        client.sendEditTask(editTask);
+
+        spanInactive.style.display = "";
+        spanEdit.style.display = "none";
+    }, false);
+
+
+    cancelBtn.addEventListener("click", function() {
+        var spanInactive = document.getElementById(fieldId + "-val-inactive");
+        var spanEdit = document.getElementById(fieldId + "-form");
+
+        spanInactive.style.display = "";
+        spanEdit.style.display = "none";
+    }, false);
+}
