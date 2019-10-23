c6febbf3744fe3d05c6c1189519a863575107de2
sa <szymon.apolinarski@gmail.com>
2019-10-23 19:35:12 +0200

    jira ticket

---
+++ jira-ticket.js
@@ -0,0 +1,17 @@
+class JiraTicket {
+    constructor(taskId, summary, description, assignee, label, dueDate) {
+        this.fields = {};
+        this.fields.project = {};
+        this.fields.project.key = "DMS";
+        this.fields.parent = {};
+        this.fields.parent.key = "DMS-8793";
+        this.fields.summary = summary;
+        this.fields.description = description;
+        this.fields.issuetype = {};
+        this.fields.issuetype.id = "10501";
+        this.fields.labels = [label];
+        this.fields.dueDate = dueDate;
+        this.fields.customfield_10904 = taskId;
+        this.fields.customfield_11700 = {"name": assignee};
+    }
+}
