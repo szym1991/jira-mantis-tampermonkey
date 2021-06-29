175a7bc0f172619fe07017ee23fd782c40bd7403
Szymon Apolinarski <szymon.apolinarski@gmail.com>
2021-06-29 07:32:13 +0200

    taskId uncomment

--- jira-ticket.js
+++ jira-ticket.js
@@ -9,14 +9,14 @@ class JiraTicket {
         if (asChanges) { // issueType === "Propozycja CR"
             this.fields.issuetype.id = "11400"; //Change Request
 //            this.fields.customfield_12500 = taskId; //nr_zgloszenia_changes
-//            this.fields.customfield_10904 = taskId; //nr_zgloszenia
+            this.fields.customfield_10904 = taskId; //nr_zgloszenia
             this.fields.customfield_10104 = Number.parseFloat(taskId); //MantisNumber
             this.fields.customfield_10105 = "https://mantis.atechno.pl/tfms/view.php?id=" + Number.parseFloat(taskId); //MantisURL
         } else {
             this.fields.parent = {};
             this.fields.parent.key = "";
             this.fields.issuetype.id = "10501"; //Bug - subtask
-//            this.fields.customfield_10904 = taskId; //nr_zgloszenia
+            this.fields.customfield_10904 = taskId; //nr_zgloszenia
             this.fields.customfield_10104 = Number.parseFloat(taskId); //MantisNumber
             this.fields.customfield_10105 = "https://mantis.atechno.pl/tfms/view.php?id=" + Number.parseFloat(taskId); //MantisURL
             // TFMS Issue Kind
